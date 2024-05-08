7473b7295238747288cef991c0fc2d14748afe7f
EzequielBonilla <ezequiel.bonilla92@gmail.com>
2024-05-07 22:48:38 -0300

    actualizado para hmwk c5

--- C5 Fundamentos Javascript/ejercicios/01.js
+++ C5 Fundamentos Javascript/ejercicios/01.js
@@ -1,6 +1,8 @@
 // Crea una variable de tipo string.
 // Reemplaza el valor de null por el correspondiente.
 
+//comentario para comitear
+
 const nuevoString = null;
 
 module.exports = nuevoString;

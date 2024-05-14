c09017f0a9f8c636636b3c64f29f8f05dcb54ded
Ezequiel Bonilla <ezequiel.bonilla92@gmail.com>
2024-05-14 11:06:40 -0300

    comentarios agregados a ejercicios

--- C6 Bucles/ejercicios/23.js
+++ C6 Bucles/ejercicios/23.js
@@ -7,17 +7,27 @@ function esNumeroPrimo(numero) {
   // solo es divisible por sí mismo y por 1.
   // Tu código:
 
-  i = 2
-  do{
+  /* i = 2 */
+  /* do{
     if(numero === 2){
       return true
     } else if(numero %i === 0) {
       return false
     } else i++
   } while(i<numero)
+    return true */
+  //con "for" creo que queda más limpio
+
+  if (numero===2){
     return true
-
-
+  }
+  /* if (numero %2 === 0) {
+    return false
+  } */
+  for(i=2 /*i=3*/; i<numero /* Math.sqrt(numero) */; i++/*i+=2*/) {
+    if(numero %i === 0) {
+      return false
+  } 
+} return true
 }
-
 module.exports = esNumeroPrimo;

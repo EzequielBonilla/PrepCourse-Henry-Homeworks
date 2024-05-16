d3c8a692a11b0769e6773e7f755b8e928a14735b
Ezequiel Bonilla <ezequiel.bonilla92@gmail.com>
2024-05-16 18:17:01 -0300

    ejercitando m7

--- C7 Arrays/ejercicios/06.js
+++ C7 Arrays/ejercicios/06.js
@@ -1,6 +1,19 @@
 function invertirArray(array) {
   // Invierte el arreglo array recibido por argumento.
   // Tu código:
+
+  var yarra = []
+  for(i = array.length - 1; i >= 0; i--) {
+    yarra.push(array[i])
+    } 
+ 
+  array = yarra //o devuelvo yarra directamente
+  return array
+  // console.log(array)
+ 
+
+  //existe lo siguiente
+  // return array.reverse()
 }
 
 module.exports = invertirArray;

2e60e4f239f88f8d882cf9f925609d92ae3ec46a
Ezequiel Bonilla <ezequiel.bonilla92@gmail.com>
2024-05-17 18:24:40 -0300

    avanzando ejercicios m7

--- C7 Arrays/ejercicios/07.js
+++ C7 Arrays/ejercicios/07.js
@@ -2,6 +2,17 @@ function ordenarArray(array) {
   // Ordena los elementos del areglo array de menor a mayor.
   // Devuelve el arreglo resultante.
   // Tu código:
+
+  for (var i=0; i<array.length-1; i++) {
+      for (var posicion=0; posicion<array.length-1; posicion++) {
+        if (array[posicion]>array[posicion+1]) {
+          var mover = array[posicion]
+          array[posicion]=array[posicion+1]
+          array[posicion+1]=mover
+        }
+      }
+  }
+  return array
 }
 
 module.exports = ordenarArray;

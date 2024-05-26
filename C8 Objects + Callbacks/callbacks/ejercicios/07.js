d1c4d8598c032f1e246073895422474badc252b5
EzequielBonilla <ezequiel.bonilla92@gmail.com>
2024-05-26 19:12:09 -0300

    callbacks

--- C8 Objects + Callbacks/callbacks/ejercicios/07.js
+++ C8 Objects + Callbacks/callbacks/ejercicios/07.js
@@ -2,6 +2,19 @@ function filter(arrayOfStrings) {
    // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
    // Luego retorna un nuevo arreglo con estos elementos.
    // Tu código:
+
+   function comienzaLetraA(string) {
+      return string.startsWith("a");
+   };
+
+   var arrayConA =[]
+   for(i=0; i<arrayOfStrings.length; i++) {
+      if (comienzaLetraA(arrayOfStrings[i])) {
+         arrayConA.push(arrayOfStrings[i]);
+      }
+   }
+
+   return arrayConA
 }
 
 module.exports = filter;
